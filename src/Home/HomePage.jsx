61bfe2e614b96fea564c14d4539f14f0f4b65898
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-05 22:14:40 +0530

    Defer below-fold home rendering

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,23 +1,53 @@
-import React from 'react'
-import Hero from './Hero'
-import FeaturedLanguages from './FeaturedLanguages'
-import ActiveRooms from './ActiveRooms'
-import HowItWorks from './HowItWorks'
-import AiUnlockSection from './AiUnlockSection'
-import Testimonials from './Testimonials'
-import SupportedBy from './SupportedBy'
+import React from 'react';
+import Hero from './Hero';
+import FeaturedLanguages from './FeaturedLanguages';
+import ActiveRooms from './ActiveRooms';
+import HowItWorks from './HowItWorks';
+import AiUnlockSection from './AiUnlockSection';
+import Testimonials from './Testimonials';
+import SupportedBy from './SupportedBy';
+
+const DeferredPaint = ({ children, size = '760px' }) => (
+  <div
+    style={{
+      contentVisibility: 'auto',
+      containIntrinsicSize: size,
+    }}
+  >
+    {children}
+  </div>
+);
+
 const HomePage = () => {
   return (
-    <div>        
-      <Hero></Hero>
-      <FeaturedLanguages></FeaturedLanguages>
-      <ActiveRooms></ActiveRooms>
-      <HowItWorks></HowItWorks>
-      <AiUnlockSection></AiUnlockSection>
-      <Testimonials></Testimonials>
-      <SupportedBy></SupportedBy>
-    </div>
-  )
-}
+    <div>
+      <Hero />
 
-export default HomePage
+      <DeferredPaint size="700px">
+        <FeaturedLanguages />
+      </DeferredPaint>
+
+      <DeferredPaint size="900px">
+        <ActiveRooms />
+      </DeferredPaint>
+
+      <DeferredPaint size="800px">
+        <HowItWorks />
+      </DeferredPaint>
+
+      <DeferredPaint size="980px">
+        <AiUnlockSection />
+      </DeferredPaint>
+
+      <DeferredPaint size="700px">
+        <Testimonials />
+      </DeferredPaint>
+
+      <DeferredPaint size="620px">
+        <SupportedBy />
+      </DeferredPaint>
+    </div>
+  );
+};
+
+export default HomePage;
