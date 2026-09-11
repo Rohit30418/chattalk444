7ffe7d1f2f27eaccefd861686f16ec0432b93f70
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-11 12:14:30 +0530

    Restore direct Connect button on rooms page

--- src/components/AppBody/Mainbody.jsx
+++ src/components/AppBody/Mainbody.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import HomeBody from './HomeBody';
+import SocialNav from '../social/SocialNav';
 
 const Mainbody = () => (
   <div className="min-h-screen">
+    <SocialNav />
     <HomeBody />
   </div>
 );
