67011c7d7706b56579e7818dab7ac47dece17488
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-14 14:43:59 +0530

    Register Vaani PWA service worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import installPeerAutoReconnect from './services/peerAutoReconnect'
+import { registerVaaniPwa } from './services/pwa'
 import './styles/index.css'
 import './styles/room-theme.css'
 import './styles/header-layout.css'
 
 installPeerAutoReconnect()
+registerVaaniPwa()
 
 const storedTheme = localStorage.getItem('theme')
 const prefersDark = window.matchMedia?.('(prefers-color-scheme: dark)').matches
