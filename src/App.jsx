1e02c4e75f357a4b73759437caf183db5c368272
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-14 18:11:29 +0530

    Mount in-app notification center

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Layout from './Layout';
 import GradientSpinner from './components/common/GradientSpinner';
 import GlobalMemberVisuals from './components/common/GlobalMemberVisuals';
 import PwaManager from './components/common/PwaManager';
+import InAppNotifications from './components/common/InAppNotifications';
 import ErrorBoundary from './ErrorBoundary';
 
 const CHUNK_RELOAD_KEY = 'vaani_chunk_reload_attempt';
@@ -185,6 +186,7 @@ function App() {
       <AuthProvider>
         <GlobalMemberVisuals />
         <PwaManager />
+        <InAppNotifications />
 
         <ToastContainer
           theme="colored"
