78ce03369a3e90c50c7312a8bf0d5e07fe234c32
Rohit Pant <80569070+Rohit30418@users.noreply.github.com>
2026-09-15 11:41:49 +0530

    Align rooms content with mobile PWA app header

--- src/components/AppBody/Mainbody.jsx
+++ src/components/AppBody/Mainbody.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import HomeBody from './HomeBody';
 import SocialNav from '../social/SocialNav';
+import useMobilePwaMode from '../../hooks/useMobilePwaMode';
 
-const Mainbody = () => (
-  <div className="min-h-screen">
-    <SocialNav />
-    <HomeBody />
-  </div>
-);
+const Mainbody = () => {
+  const isMobilePwa = useMobilePwaMode();
+
+  return (
+    <div className="min-h-screen">
+      <SocialNav />
+      <div className={isMobilePwa ? '-mt-[86px]' : ''}>
+        <HomeBody />
+      </div>
+    </div>
+  );
+};
 
 export default Mainbody;
